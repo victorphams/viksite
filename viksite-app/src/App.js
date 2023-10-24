aeb44d766dfe62c8afc47414edcb3a298a4a660c
victorphams <victor.phamk@gmail.com>
2023-10-23 20:01:20 -0500

    Just about page

--- viksite-app/src/App.js
+++ viksite-app/src/App.js
@@ -17,8 +17,7 @@ function App() {
       <Link to="/about">About</Link>
     </nav> */}
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About />}/>
+      <Route path="/" element={<About />}/>
    </Routes>
 </BrowserRouter>
  )
