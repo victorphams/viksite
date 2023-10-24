6aa441ec17e7e7982edc38d7c39acf982d939743
victorphams <victor.phamk@gmail.com>
2023-10-23 23:06:52 -0500

    Added Responsive Design

--- src/App.js
+++ src/App.js
@@ -36,28 +36,31 @@ function Home () {
 function About() {
   return (
       <body>
-      <div class="main">
-        <div class="info">
-          <h1 class="title">Victor Pham</h1>
-          <p class="subtitle">Oklahoma City, OK</p>
-          <p class="subtitle">Heya, I'm Vik and love doing a lot of things! I'm a recent computer science graduate from the University of Oklahoma. </p>
-        </div>
-
-        <div class="midsection"></div>
-
-        <div class="links">
-          <a href='mailto:victor.phamk@gmail.com'>email</a><br></br>
-          <br></br>
-          <a href='https://github.com/victorphams' target="_blank">github</a><br></br>
-          <br></br>
-          <a href='https://linkedin.com/in/victorphams' target="_blank">linkedin</a><br></br>
-          <br></br>
+      <div class="h-screen flex items-center">
+      
+        <div class="main container items-center w-5/6">
+          <div class="info space-y-5">
+            <h1 class="title font-bold text-3xl">Victor Pham</h1>
+            <p class="subtitle">Oklahoma City, OK</p>
+            <p>Heya! I'm Vik and love doing a lot of things! I'm a recent computer science graduate from the University of Oklahoma. </p>
           
-          
-        </div>
+            <div class="links">
+              <a href='mailto:victor.phamk@gmail.com'>email</a><br></br>
+              <br></br>
+              <a href='https://github.com/victorphams' target="_blank">github</a><br></br>
+              <br></br>
+              <a href='https://linkedin.com/in/victorphams' target="_blank">linkedin</a><br></br>
+              <br></br>
+           </div>
+          </div>
+
+        {/* <div class="midsection"></div> */}
+
+       
 
         
-      </div>
+      </div></div>
+      
       <img src={waves} className="waves"/>
     </body>
 
